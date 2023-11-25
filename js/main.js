05f397f52e05f375d7af7be0420aebd7c9db5d2c
tomy-web-cordinator <132113380+tomy-web-coordinator@users.noreply.github.com>
2023-11-25 10:18:24 +0900

    drawerオープン時にbodyにis-fixed付与を追加

--- js/main.js
+++ js/main.js
@@ -14,14 +14,16 @@
   switchViewport();
 })();
 
-// drawer-open-close
+// ドロワーの開閉
 jQuery("#js-drawer-icon").on("click", function (e) {
   e.preventDefault();
+  jQuery("body").toggleClass("is-fixed");
   jQuery("#js-drawer-icon").toggleClass("is-checked");
   jQuery("#js-drawer-content").toggleClass("is-checked");
 });
 
 jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  jQuery("body").removeClass("is-fixed");
   jQuery("#js-drawer-icon").removeClass("is-checked");
   jQuery("#js-drawer-content").removeClass("is-checked");
 });
